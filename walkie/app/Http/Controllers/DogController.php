452cb368364f08563ed129126a341634983cb905
marcell <ciszekmarcell@gmail.com>
2019-02-25 17:11:30 +0100

    form validation and a form setup

---
+++ walkie/app/Http/Controllers/DogController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Dog;
+
+class DogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $breeds = \App\Breed::pluck('name', 'id');
+
+        return view('create_dog/create_dog', compact(['breeds']));
+        return view('create_dog.create_dog');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $attributes = request()->validate([
+            'breed_id' => 'required',
+            'name' => ['required', 'min:2', 'max:50'],
+            'age' => 'required',
+            'sex' => 'required',
+            'description' => ['required', 'min:10', 'max:255'],
+        ]);
+
+        $attributes['breed_id'] = auth()->id();
+        Dog::create($attributes);
+
+        return redirect('/walkie');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- walkie/routes/web.php
+++ walkie/routes/web.php
@@ -10,15 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/dog-create', function () 
-{
+// Route::get('/dog-create', function ()
+// {
 
-    // return \App\Breed::all();
+    // $breeds = \App\Breed::pluck('name', 'id');
 
-    $breeds = \App\Breed::pluck('name', 'id');
-    
-    return view('/create_dog/create_dog', compact(['breeds']));
-});
-// Route::get('/{path?}', function () {
-//     return view('welcome');
+    // return view('/create_dog/create_dog', compact(['breeds']));
 // });
+
+    Route::resource('/walkie', 'DogController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
