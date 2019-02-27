57c00f59278923b2c5e2638a994e68d33ce15095
marcell <ciszekmarcell@gmail.com>
2019-02-27 16:19:08 +0100

    adding column user name in the user table

--- walkie/database/migrations/2014_10_12_000000_create_users_table.php
+++ walkie/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('last_name', 40);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('user_name')->unique();
             $table->string('password');
             $table->rememberToken();
 
