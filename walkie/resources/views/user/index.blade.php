33ea19e3534c68b6406cd9cf619615ed2c239b03
wonderland <wonderland@wonderland.local>
2019-03-22 14:56:04 +0100

    commit

--- walkie/resources/views/user/index.blade.php
+++ walkie/resources/views/user/index.blade.php
@@ -4,10 +4,13 @@
 
 
 <h1 class="user-title">Welcome</h1>
-    <div class="container">
-        <div class="jumbotron" id="user_info_container">
-            <div class="card">
-                <div class="card-body">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="jumbotron" id="user_info_container">
+                <div class="card">
+                    <div class="card-body" id="user-info-card">
                     <h3>{{$auth->first_name}} {{$auth->last_name}} </h3>
                     <p>{{$auth->user_name}}</p>
                     <p>{{$auth->email}}</p>
@@ -15,17 +18,17 @@
                     <p>{{$auth->age}}</p>
                     <p>{{$auth->gender}}</p>
                     <a href="{{action('UserController@edit',$auth->id)}}" class="btn btn-danger">Update Profile</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="container">
-        <div class="jumbotron" id="user_info_container">
-            <div class="card">
-                <div class="card-body">
+        <div class="col-md-6">
+            <div class="jumbotron" id="user_info_container">
+                <div class="card">
+                    <div class="card-body">
                         <h2>Previous Walks:</h2>
                         @foreach ($prevWalks as $walk)
-                <p> {{ $walk->date}} - {{ $walk->hour}}:00 with {{ $walk->dog->name}}</p>
+                        <p> {{ $walk->date}} - {{ $walk->hour}}:00 with {{ $walk->dog->name}}</p>
                         @endforeach
 
 
@@ -34,10 +37,12 @@
                             <p> {{ $walk->date}} - {{ $walk->hour}}:00 with {{ $walk->dog->name}}</p>
                         @endforeach
                 
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 
 @endsection
